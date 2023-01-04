a4dfef028d66a4aad8e176bf09c29ddcda2c83b0
henricsoares <henricsoares@gmail.com>
2023-01-04 10:50:21 -0300

    17-3

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,6 +2,7 @@
 Test for models.
 """
 from decimal import Decimal
+from unittest.mock import patch
 
 from core import models
 from django.contrib.auth import get_user_model
@@ -70,6 +71,15 @@ class ModelTests(TestCase):
     def test_create_ingredient(self):
         """Test creating an ingredient is successful."""
         user = create_user()
-        ingredient = models.Ingredient.objects.create(user=user, name="Ingredient")
+        ingredient = models.Ingredient.objects.create(user=user, name="Ingredient1")
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    @patch("core.models.uuid.uuid4")
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """Test generating image path."""
+        uuid = "test-uuid"
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, "example.jpg")
+
+        self.assertEqual(file_path, f"uploads/recipe/{uuid}.jpg")
